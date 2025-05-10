b5472e55b8a5e7e56ec1fbc1e720e1d6ded20ffe
s-i-m-o-n <ptifuze6212@gmail.com>
2025-05-10 03:06:53 +0200

    Add Logs.h

---
+++ Logs.h
@@ -0,0 +1,10 @@
+#ifndef DISCORD_LOGS_H
+#define DISCORD_LOGS_H
+
+#include <string>
+
+
+
+void SendLogs(const std::string& Content, const std::string& Username, const std::string& Title, const std::string& Description);
+
+#endif // DISCORD_LOGS_H
